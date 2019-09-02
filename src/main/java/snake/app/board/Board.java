de0b3c11c0e2d28b0cd555d02d9a868c6debc438
Alisher <31369395+Aurrix@users.noreply.github.com>
2019-09-02 16:23:35 +0300

    Board

--- src/main/java/snake/app/board/Board.java
+++ src/main/java/snake/app/board/Board.java
@@ -1,5 +1,6 @@
 package snake.app.board;
 
+import snake.app.body.Apple;
 import snake.app.body.SnakeBody;
 import snake.app.directions.Directions;
 
@@ -8,7 +9,9 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 
 public class Board extends JPanel {
@@ -18,8 +21,10 @@ public class Board extends JPanel {
     public Dimension blockSize = new Dimension();
     private Thread boardThread;
     List<SnakeBody> snake = new ArrayList<>();
-    public Directions dir = Directions.RIGHT;
+    List<Apple> foods = new ArrayList<>();
     public Directions pressed = Directions.RIGHT;
+    JLabel score;
+
 
 
     public Board() {
@@ -30,16 +35,16 @@ public class Board extends JPanel {
             @Override
             public void keyPressed(KeyEvent e) {
 //                System.out.println(e);
-                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                if (e.getKeyCode() == KeyEvent.VK_UP && pressed != Directions.DOWN) {
                     pressed = Directions.TOP;
                 }
-                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                if (e.getKeyCode() == KeyEvent.VK_DOWN && pressed != Directions.TOP) {
                     pressed = Directions.DOWN;
                 }
-                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                if (e.getKeyCode() == KeyEvent.VK_LEFT && pressed != Directions.RIGHT) {
                     pressed = Directions.LEFT;
                 }
-                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT && pressed != Directions.LEFT) {
                     pressed = Directions.RIGHT;
                 }
             }
@@ -47,16 +52,16 @@ public class Board extends JPanel {
         setFocusable(true);
         requestFocusInWindow();
         snake.clear();
+        foods.clear();
         snake.add(new SnakeBody(true, new Point(2, 0), new Point(2, 0)));
         snake.add(new SnakeBody(false, new Point(1, 0), new Point(1, 0)));
         snake.add(new SnakeBody(false, new Point(0, 0), new Point(0, 0)));
         updateBlockSize();
-
+        pressed = Directions.RIGHT;
         // LOOP THREAD
 
         boardThread = new Thread(() -> {
             while (!gameOver) {
-
                 try {
                     boardThread.sleep(TICK);
                 } catch (InterruptedException e) {
@@ -72,28 +77,22 @@ public class Board extends JPanel {
                         //System.out.println("Head "+i);
                         switch (pressed) {
                             case TOP:
-                                if (dir != Directions.DOWN) {
-                                    dir = pressed;
-                                    snake.get(i).setPosition(new Point(current.x, current.y - 1));
-                                }
+                                snake.get(i).setPosition(new Point(current.x, current.y - 1));
                                 break;
                             case DOWN:
-                                if (dir != Directions.TOP) {
-                                    dir = pressed;
+
                                     snake.get(i).setPosition(new Point(current.x, current.y + 1));
-                                }
+
                                 break;
                             case LEFT:
-                                if (dir != Directions.RIGHT) {
-                                    dir = pressed;
+
                                     snake.get(i).setPosition(new Point(current.x - 1, current.y));
-                                }
+
                                 break;
                             case RIGHT:
-                                if (dir != Directions.LEFT) {
-                                    dir = pressed;
+
                                     snake.get(i).setPosition(new Point(current.x + 1, current.y));
-                                }
+
                                 break;
                         }
 
@@ -107,9 +106,39 @@ public class Board extends JPanel {
 
                 }
                 checkForGameOver();
+                checkForFoodEaten();
                 repaint();
             }
         });
+        
+        generateOneFood();
+        
+    }
+
+    private void checkForFoodEaten() {
+        Iterator<SnakeBody> snakeIterator = snake.iterator();
+        Iterator<Apple> foodIterator = foods.iterator();
+        List<SnakeBody> tempSnake = new ArrayList<>();
+        List<Apple> tempFood = new ArrayList<>();
+        while(foodIterator.hasNext()){
+            Apple currentApple = foodIterator.next();
+            while(snakeIterator.hasNext()){
+                SnakeBody currentBody = snakeIterator.next();
+                if (currentApple.getPosition().equals(currentBody.getPosition())){
+                    Point position = snake.get(snake.size()-1).getPreviousPosition();
+                    tempSnake.add(new SnakeBody(false,position,position));
+                    foodIterator.remove();
+                    tempFood.add(new Apple(new Point((int)(Math.random()*blockSize.width),(int)(Math.random()*blockSize.height))));
+                }
+            }
+        }
+        snake.addAll(tempSnake);
+        foods.addAll(tempFood);
+        score.setText(String.valueOf(Integer.valueOf(score.getText())+tempSnake.size()));
+    }
+
+    private void generateOneFood() {
+        foods.add(new Apple(new Point((int)(Math.random()*blockSize.width),(int)(Math.random()*blockSize.height))));
     }
 
     private void checkForGameOver() {
@@ -124,7 +153,6 @@ public class Board extends JPanel {
                 gameOver = true;
             }
         }
-
 
     }
 
@@ -147,7 +175,6 @@ public class Board extends JPanel {
             }
         }
     }
-
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -155,5 +182,17 @@ public class Board extends JPanel {
         for (SnakeBody body : snake) {
             g.fillRect(blockSize.width * body.getPosition().x, blockSize.height * body.getPosition().y, blockSize.width - 1, blockSize.height - 1);
         }
+        g.setColor(Color.GREEN);
+        for (Apple food : foods) {
+            g.fillRect(blockSize.width * food.getPosition().x, blockSize.height * food.getPosition().y, blockSize.width - 1, blockSize.height - 1);
+        }
+    }
+
+    public JLabel getScore() {
+        return score;
+    }
+
+    public void setScore(JLabel score) {
+        this.score = score;
     }
 }
